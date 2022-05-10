75fa3c1325fca6d122d43d33b16db655d40ec129
funderburkjim <funderburkjim@gmail.com>
2022-05-10 17:24:57 -0400

    Visarga correction. Ref: https://github.com/sanskrit-lexicon/BUR/issues/3#issuecomment-1122711774

---
+++ issue3/prep1a.py
@@ -0,0 +1,96 @@
+#-*- coding:utf-8 -*-
+"""prep1a.py
+ 
+"""
+from __future__ import print_function
+import sys,re,codecs
+import digentry  
+
+
+class Change:
+ def __init__(self,metaline,lnum,old,new):
+  self.metaline = metaline
+  self.lnum = lnum
+  self.old = old
+  self.new = new
+
+def change_line_lnum(lnum,line):
+ d = {
+  663: ('agrata(s)sara','agratassara'),
+  18798: ('(s)','{%s%}'),
+  31161:('pṛṣṭhata(s)','pṛṣṭhatas'),
+  73410:('sukhadu(s)kānām','sukhaduskānām'),  
+  79159:('yata(s) pravṛtti(s)','yatas pravṛttis'),
+  86888: ('mana(s)prasāda','manasprasāda'),
+  86890: ('mana(s)śila','manasśila'), 
+  86892: ('mana(s)sila','manassila'), 
+  88419: ('bhuva(s)','bhuvas'),   
+ 107281: ('vilubhila(s)','vilubhilas'), 
+ 114875: ('daṇḍa(s)','daṇḍas'),  
+ 119369: ('śloka(s)','ślokas'), 
+ 119371: ('divya(s) pāyu(s)','divyas pāyus'), 
+ 120094: ('sadya(s)śotā','sadyasśotā'), 
+ 122503: ('aha(s)','ahas'), 
+ 125035: ('ka(s)','kas'), 
+ 131758: ('eka(s)','ekas'),  
+ 132038: ('śira(s)','śiras'),
+ 132470: ('āpa(s)','āpas'),
+ 134781: ('ahā(s)','ahās'), 
+ 134788: ('jabhru(s)','jabhrus'),
+  
+ }
+ if lnum not in d:
+  return line
+ old1,old = d[lnum]
+ new = old1.replace('(s)','ḥ')
+ newline = line.replace(old,new)
+ return newline
+
+def get_changes(entries):
+ changes = []
+ for entry in entries:
+  metaline = entry.metaline
+  lnum0 = entry.linenum1
+  for iline,line in enumerate(entry.datalines):
+   lnum = lnum0 + iline + 1
+   newline = change_line_lnum(lnum,line)
+   if newline != line:
+    lnum = lnum0 + iline + 1
+    change = Change(metaline,lnum,line,newline)
+    changes.append(change)
+ print(len(changes),'Change records generated')
+ return changes
+
+def write_changes(fileout,changes):
+ outrecs = []
+ for change in changes:
+  outarr = []
+  outarr.append('; =====================================================')
+  metaline = change.metaline
+  metaline = re.sub(r'<k2>.*$','',metaline)
+  outarr.append('; %s' % metaline)
+  lnum = change.lnum
+  old = change.old
+  new = change.new
+  outarr.append('%s old %s' %(lnum,old))
+  outarr.append(';')
+  outarr.append('%s new %s' %(lnum,new))
+  outrecs.append(outarr)
+  
+ with codecs.open(fileout,"w","utf-8") as f:
+  for outarr in outrecs:
+   for line in outarr:
+    f.write(line+'\n')
+ print(len(outrecs),"records written to",fileout)
+
+
+if __name__=="__main__":
+ filein = sys.argv[1] # bur csl-orig
+ fileout = sys.argv[2] #  
+
+ entries = digentry.init(filein)
+ #Ldict = digentry.Entry.Ldict
+
+ changes = get_changes(entries)
+ write_changes(fileout,changes)
+ 
--- issue3/readme.txt
+++ issue3/readme.txt
@@ -12,7 +12,7 @@ temp_bur_1.txt  manual corrections
 python prep1.py temp_bur_0.txt change_1.txt
 python updateByLine.py temp_bur_0.txt change_1.txt temp_bur_1.txt
 
-94 changes ạ, Ạ to <ab>a2.</ab> or <ab>A2.</ab>
+94 changes áº¡, áº  to <ab>a2.</ab> or <ab>A2.</ab>
 1  A'ler -> Aller (k1 = stfkz)
 22 (s) -> s:   Some of these are odd, and would be visarga e.g. duskham
 1  8c -> 8e  (8th)
@@ -40,5 +40,21 @@ sh xmlchk_xampp.sh bur
  # ok  commited changes to csl-orig. 05-10-2022.
 
 ==============================================================
+change_1a:  (s) -> ḥ
+Andhrabharati points out there is a typographical distinction
+between a 'broken-s' (visarga) and a normal s in Burnouf
+(Ref: https://github.com/sanskrit-lexicon/BUR/issues/3#issuecomment-1122711774)
+I presume that all the '(s) -> s' changes of change_1 need to
+be corrected.
+That's what change_1a does.
+python prep1a.py temp_bur_1.txt change_1a.txt
 ==============================================================
+python updateByLine.py temp_bur_1.txt change_1a.txt temp_bur_1a.txt
+cp temp_bur_1a.txt /c/xampp/htdocs/cologne/csl-orig/v02/bur/bur.txt
+cd /c/xampp/htdocs/cologne/csl-pywork/v02
+sh generate_dict.sh bur  ../../bur
+sh xmlchk_xampp.sh bur   
+ # ok  commited changes to csl-orig. 05-10-2022.
+
+cd /c/xampp/htdocs/sanskrit-lexicon/bur/issue3
 ==============================================================
